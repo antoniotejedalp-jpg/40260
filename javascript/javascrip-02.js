86433f296a3b8e512ca5c329e706b23a65e1e130
antoniotejeda-jpg <antoniotejedalp@gmail.com>
2026-05-08 11:21:03 -0600

    tarea calculadora

--- javascript/javascrip-02.js
+++ javascript/javascrip-02.js
@@ -1,5 +1,8 @@
 let resultado = document.getElementById("resultado");
 let botones = document.getElementsByTagName("button");
+let prm1 = 0;
+let operador = "";
+
 for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
         const boton = botones[key];
@@ -9,33 +12,56 @@ for (const key in botones) {
         else {
             boton.addEventListener("click", pintar2);
         }
-        console.log(boton.className);
     }
 }
 
 function pintar(e) {
-    console.log(e.target.innerText);
     resultado.value += e.target.innerText;
 }
 
-
-
-
-
 function pintar2(e) {
-    console.log(e.target.innerText)
-    if (e.target.innerText == "+") {
-        suma()
+    let tecla = e.target.innerText;
+    
+    if (tecla === "Ce") {
+        resultado.value = "";
+        prm1 = 0;
+        operador = "";
+    }
+    else if (tecla === "=") {
+        igual();
+    }
+    else {
+        // Es un operador (+, -, *, /)
+        if (resultado.value !== "") {
+            prm1 = resultado.value;
+            operador = tecla;
+            resultado.value = "";
+        }
     }
 }
 
-let prm1 = 0;
-function suma() {
-    prm1 = resultado.value
-    resultado.value = ""
-}
-
 function igual() {
-    let prm2 = resultado.value
-    resultado.value = parseInt(prm1) + parseInt(prm2)
+    if (operador === "" || resultado.value === "") return;
+    
+    let prm2 = resultado.value;
+    let res = 0;
+    
+    switch (operador) {
+        case "+":
+            res = parseInt(prm1) + parseInt(prm2);
+            break;
+        case "-":
+            res = parseInt(prm1) - parseInt(prm2);
+            break;
+        case "*":
+            res = parseInt(prm1) * parseInt(prm2);
+            break;
+        case "/":
+            res = parseInt(prm1) / parseInt(prm2);
+            break;
+    }
+    
+    resultado.value = res;
+    operador = "";
+    prm1 = 0;
 }
